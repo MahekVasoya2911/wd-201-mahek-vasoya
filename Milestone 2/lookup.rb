cb5a5fa08577042d68c667ea3f41ea5ed9d6437e
MahekVasoya2911 <mahekvasoya2911@gmail.com>
2021-06-21 10:04:43 +0530

    Milestone 2 commit

---
+++ Milestone 2/lookup.rb
@@ -0,0 +1,51 @@
+def get_command_line_argument
+  # ARGV is an array that Ruby defines for us,
+  # which contains all the arguments we passed to it
+  # when invoking the script from the command line.
+  # https://docs.ruby-lang.org/en/2.4.0/ARGF.html
+  if ARGV.empty?
+    puts "Usage: ruby lookup.rb <domain>"
+    exit
+  end
+  ARGV.first
+end
+
+# `domain` contains the domain name we have to look up.
+domain = get_command_line_argument
+
+# File.readlines reads a file and returns an
+# array of string, where each element is a line
+# https://www.rubydoc.info/stdlib/core/IO:readlines
+dns_raw = File.readlines("zone")
+
+def parse_dns(dns_raw)
+  dns_raw.reject { |x| (x[0] == "#") || (x[0] == "\n") }.
+    map { |x| x.strip().split(", ") }.
+    each_with_object({}) do |record, records|
+    records[record[1]] = { type: record[0], target: record[2] }
+  end
+end
+
+def resolve(dns_records, lookup_chain, domain)
+  record = dns_records[domain]
+  if (!record)
+    lookup_chain.pop()
+    lookup_chain << "Error: record not found for " + domain
+  elsif record[:type] == "CNAME"
+    lookup_chain.append(record[:target])
+    resolve(dns_records, lookup_chain, lookup_chain.last)
+  elsif record[:type] == "A"
+    lookup_chain.append(record[:target])
+  else
+    lookup_chain.pop()
+    lookup_chain << "Invalid record type for " + domain
+  end
+end
+
+# To complete the assignment, implement `parse_dns` and `resolve`.
+# Remember to implement them above this line since in Ruby
+# you can invoke a function only after it is defined.
+dns_records = parse_dns(dns_raw)
+lookup_chain = [domain]
+lookup_chain = resolve(dns_records, lookup_chain, domain)
+puts lookup_chain.join(" => ")
